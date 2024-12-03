3a3340618cf27382e06e6f2d6bf159363301709b
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2024-12-03 18:20:00 +0800

    tijiao

---
+++ upm.bat
@@ -0,0 +1,23 @@
+@echo off
+set b="version"
+set version ="1"
+REM 获取版本号
+for /f "tokens=1,2* delims=:," %%a in (Assets/ActionEditor/package.json) do (
+    echo %%a| findstr %b% >nul && (
+       set version=  %%b
+    ) || (
+        @REM echo %%a nnn %b%
+    )
+)
+
+
+set version=%version: =%
+echo on
+git subtree split --prefix=Assets/ActionEditor --branch upm
+git push origin upm:upm
+git tag %version% upm
+git push origin upm --tags
+set cur=%~dp0
+
+
+pause
