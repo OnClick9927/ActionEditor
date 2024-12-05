8e69f79bf34daa2bf9ca9e3daf32fbead2b61e6e
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2024-12-05 10:38:48 +0800

    tijiao

--- Editor/GUIS/PreferencesWindow.cs
+++ Editor/GUIS/PreferencesWindow.cs
@@ -4,34 +4,27 @@ using UnityEngine;
 
 namespace ActionEditor
 {
-    public class PreferencesWindow : PopupWindowContent
+    class PreferencesWindow : PopupWindowContent
     {
-        private static Rect _myRect;
-        private bool firstPass = true;
+        //private static Rect _myRect;
+        //private bool firstPass = true;
 
         public static void Show(Rect rect)
         {
-            _myRect = rect;
-            PopupWindow.Show(new Rect(rect.x, rect.y, 0, 0), new PreferencesWindow());
+            rect.x = rect.x - 300 + rect.width;
+            //_myRect = rect;
+            PopupWindow.Show(rect, new PreferencesWindow());
         }
 
-        public override Vector2 GetWindowSize()
-        {
-            return new Vector2(_myRect.width, _myRect.height);
-        }
+        public override Vector2 GetWindowSize() => new Vector2(300, 150);
 
         public override void OnGUI(Rect rect)
         {
-            GUILayout.BeginVertical("box");
-            GUI.color = new Color(0, 0, 0, 0.3f);
+    
 
-            GUILayout.BeginHorizontal();
-            GUI.color = Color.white;
             GUILayout.Label($"<size=22><b>{Lan.ins.PreferencesTitle}</b></size>");
-            GUILayout.EndHorizontal();
             GUILayout.Space(2);
 
-            GUILayout.BeginVertical("box");
             var lan = EditorTools.CleanPopup<string>("Language", Lan.Language,
                 Lan.AllLanguages.Keys.ToList());
             if (lan != Lan.Language)
@@ -39,9 +32,7 @@ namespace ActionEditor
                 Lan.SetLanguage(lan);
             }
 
-            GUILayout.EndVertical();
 
-            GUILayout.BeginVertical("box");
             Prefs.timeStepMode =
                 (Prefs.TimeStepMode)EditorGUILayout.EnumPopup(Lan.ins.PreferencesTimeStepMode, Prefs.timeStepMode);
             if (Prefs.timeStepMode == Prefs.TimeStepMode.Seconds)
@@ -55,46 +46,25 @@ namespace ActionEditor
                     Prefs.frameRates.ToList());
             }
 
-            GUILayout.EndVertical();
 
-            //GUILayout.BeginVertical("box");
             Prefs.MagnetSnapping =
                 EditorGUILayout.Toggle(new GUIContent(Lan.ins.PreferencesMagnetSnapping, Lan.ins.PreferencesMagnetSnappingTips),
                     Prefs.MagnetSnapping);
-            //Prefs.scrollWheelZooms =
-            //    EditorGUILayout.Toggle(
-            //        new GUIContent(Lan.ins.PreferencesScrollWheelZooms, Lan.ins.PreferencesScrollWheelZoomsTips),
-            //        Prefs.scrollWheelZooms);
-            //GUILayout.EndVertical();
 
-            GUILayout.BeginVertical("box");
 
-            //Prefs.savePath = EditorTools.GUILayoutGetFolderPath(Lan.ins.PreferencesSavePath, Lan.ins.PreferencesSavePathTips,
-            //    Prefs.savePath, true);
+
+
 
             Prefs.autoSaveSeconds = EditorGUILayout.IntSlider(
                 new GUIContent(Lan.ins.PreferencesAutoSaveTime, Lan.ins.PreferencesAutoSaveTimeTips), Prefs.autoSaveSeconds, 5,
                 120);
-            GUILayout.EndVertical();
 
 
-            GUILayout.EndVertical();
 
-            //DrawHelpButton();
 
-            if (firstPass || Event.current.type == EventType.Repaint)
-            {
-                firstPass = false;
-                _myRect.height = GUILayoutUtility.GetLastRect().yMax + 5;
-            }
+
         }
 
-        //protected virtual void DrawHelpButton()
-        //{
-        //    if (GUILayout.Button(Lan.ins.PreferencesHelpDoc))
-        //    {
-        //        Application.OpenURL("https://nobug.cn/book/414447506088261");
-        //    }
-        //}
+
     }
 }
