3ba529b082d236f853e9b8ddf6ca26d6f59259a0
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2024-12-11 16:00:35 +0800

    处理序列化重命名

--- Runtime/Assets/Group.cs
+++ Runtime/Assets/Group.cs
@@ -126,8 +126,12 @@ namespace ActionEditor
         {
             tracks = this.Temps.ConvertAll(x =>
             {
-                return JsonUtility.FromJson(x.json, Asset.GetType(x.type)) as Track;
+                var type = Asset.GetType(x.type);
+                if (type != null)
+                    return JsonUtility.FromJson(x.json, type) as Track;
+                return null;
             });
+            tracks.RemoveAll(x => x == null);
             for (int i = 0; i < tracks.Count; i++)
             {
                 ((IDirectable)tracks[i]).AfterDeserialize();
